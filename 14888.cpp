67cd4d78f25ad22021f4232f22cf4ed11c8b5a91
정원엽 <nerbs0313@gmail.com>
2020-05-31 13:57:14 +0900

    14888 solution using by dfs algorithm

--- 14888.cpp
+++ 14888.cpp
@@ -2,3 +2,49 @@
 #include <iostream>
 #include <vector>
 #include <stdlib.h>
+
+using namespace std;
+
+int n, pluss, minuss, mul, divv;
+int minNum = 2147483648;
+int maxNum = -2147483647;
+int num[12];
+
+void dfs(int pl, int mi, int mu, int di, int index, int result) {
+	if (index == n) {
+		if (minNum > result) {
+			minNum = result;
+		}
+		if (maxNum < result) {
+			maxNum = result;
+		}
+		return;
+	}
+	if (pluss > pl) 
+	{
+		dfs(pl + 1, mi, mu, di, index + 1, result + num[index]);
+	}
+	if (minuss > mi)
+	{
+		dfs(pl, mi + 1, mu, di, index + 1, result - num[index]);
+	}
+	if (mul > mu) {
+		dfs(pl, mi, mu + 1, di, index + 1, result*num[index]);
+	}
+	if (divv > di) {
+		dfs(pl, mi, mu, di+1, index + 1, result / num[index]);
+	}
+}
+
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> num[i];
+	}
+	cin >> pluss >> minuss >> mul >> divv;
+	dfs(0, 0, 0, 0, 1, num[0]);
+
+	cout << maxNum << endl;
+	cout << minNum << endl;
+
+}
