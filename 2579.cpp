f02355f8e07d4f8360c6cec191a9309d2cdbf926
정원엽 <nerbs0313@gmail.com>
2020-07-21 09:43:20 +0900

    Dynamic Programming. 계단오르기

--- 2579.cpp
+++ 2579.cpp
@@ -20,42 +20,35 @@ int main() {
 
 	cin >> N;
 	int floor[301] = { 0, };
-	int d[301] = { 0, }; //결과
-	int d2[301] = { 0, };
-	d[0] = 0; //초기화. d[i] : i계단까지 올라온 계단 합으로 정의.
-	/*
-		계단을 한칸오르는 케이스 -> 연속해서 세칸오르는 케이스는 없다.,올라도되고,쉬어도되고, 대신에 오를시에 표시.
-		계단을 두칸오르는 케이스
+	int dp[301] = { 0, }; //결과
+	//초기화. dp[i] : i계단까지 올라온 계단 합으로 정의.
 
-		XOXOOX
-		XOOXOX
-		OXOOX
-		OOXOX
-		X를 초점으로 해서.
 
-	*/
+
 	for (int i = 1; i <= N; i++) {
 		cin >> floor[i];
 	}
+	/*
+	OXO
+	OOX
+	XOO
+	*/
+	dp[1] = floor[1];
+	dp[2] = maxx(floor[1] + floor[2], floor[2]);
+	dp[3] = maxx(floor[2] + floor[3], floor[1] + floor[3]);
 
-	for (int i = 0; i <= N - 1; i++) {
-		if ((i + 1) == 1) {
-			d[i + 1] = floor[i + 1];
-		}
-		d[i + 1] = maxx(d[i] + floor[i + 1], d[i + 1]);//XO
-		d[i + 1] = maxx(floor[i] + d[i + 1], d[i + 1]);//OX
+
+	/*
+	_______________/dp/XO
+	______________/dp/XOO
+
+	*/
+	for (int i = 4; i <= N; i++) {
+		dp[i] = maxx(dp[i - 2] + floor[i], dp[i - 3] + floor[i - 1] + floor[i]);
 	}
 
 
-	for (int i = 0; i <= N-1; i++) {
-		if ((i + 1) == 1) {
-			d2[i + 1] = 0;
-		}
-		d2[i + 1] = maxx(d2[i] + floor[i + 1], d2[i + 1]);//XO
-		d2[i + 1] = maxx(floor[i] + d2[i + 1], d2[i + 1]);//OX
-	}
 
-
-	cout << maxx(d[N], d2[N]) << endl;
+	cout << dp[N] << endl;
 	system("pause");
 }
