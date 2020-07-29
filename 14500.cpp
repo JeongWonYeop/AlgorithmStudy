9c9db16fd32096997adcb6c262a6e4b4fd9fe7ff
정원엽 <nerbs0313@gmail.com>
2020-07-29 20:52:55 +0900

    14500 테트로미노

---
+++ 14500.cpp
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <utility>
+using namespace std;
+
+/*
+각 모양별로, 가장 높은 합의 값 알아내고,
+그 중에서 가장 높은 값을 답으로 제출.
+10^8 * 2  -- > 1억짜리 이중포문 두개까지는 허용하겠다.
+*/
+
+/*
+왼쪽 위를 기준으로 하기로 약속.
+길쭉한 형태는 
+*/
+
+//ㅁㅁㅁㅁ
+void tet1(int x, int y) {
+
+}
+//ㅁㅁㅁ
+//ㅁ
+void tet2(int x,int y) {
+
+}
+//ㅁㅁ
+//ㅁㅁ
+void tet3(int x, int y) {
+
+}
+//ㅁㅁ
+//  ㅁㅁ
+void tet4(int x, int y) {
+
+
+}
+//ㅁㅁ
+//ㅁ
+//ㅁ
+void tet2(int x, int y) {
+
+}
+//ㅁ
+//ㅁㅁ
+//  ㅁ
+void tet2(int x, int y) {
+
+}
+
+
+
+
+int main() {
+
+}
