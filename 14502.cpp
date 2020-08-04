6d563fc1e04059d2bea2407a912ac641d63a59ce
정원엽 <nerbs0313@gmail.com>
2020-08-04 16:20:55 +0900

    14502 연구소 백트래킹 문제 풀이 success.

--- 14502.cpp
+++ 14502.cpp
@@ -6,8 +6,8 @@
 #include <utility>
 #include <functional>
 using namespace std;
+int makeboard[9][9] = { 0, };
 int board[9][9] = { 0, };
-int initboard[9][9] = { 0, };
 int check[9][9] = { 0, };
 int N, M;
 int dx[4] = {0,0,-1,1};//좌,우,상,하
@@ -24,39 +24,36 @@ queue <PairInt> que;
 	초 개념은 없지만 넣어서 풀어보자.
 	각 바이러스가 한칸 퍼져나갈 때 1초임.
 	bfs로 풀면됨.
+	시간초과 : 10^8 *2
 */
 
-void setting() {
-	/*
-		바로 백트래킹이로구나.
-	*/
-}
 
-
-void reset() {
+void SafeSum() { //64
+	int tempsum = 0;
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= M; j++) {
-			board[i][j] = initboard[i][j];
-			check[i][j] = initboard[i][j];
-			if (check[i][j] == -2)  check[i][j] = 0;
+			if (makeboard[i][j] == 0)
+				++tempsum;
 		}
 	}
 
-}
-
-
-void SafeSum() {
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= M; j++) {
-			if (board[i][j] == 0)
-				++safesum;
-		}
+	if (tempsum > safesum) {
+		safesum = tempsum;
 	}
 
-
 }
-
-void Debug() {
+void initboard(){ //64
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
+			makeboard[i][j] = board[i][j];
+			if (makeboard[i][j] == -2) {
+				que.push({ i,j });
+			}
+		}
+	}
+}
+/*
+void Debug() { 
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= M; j++) {
 			cout << check[i][j] << " ";
@@ -65,6 +62,59 @@ void Debug() {
 	}
 	cout << endl;
 }
+*/
+
+void BFS() {
+	initboard(); //64
+	while (!que.empty()) { //64
+		PairInt temp = que.front();
+		for (int i = 0; i < 4; i++) { //64*4 = 240
+			int nx, ny;
+			nx = temp.first + dx[i];
+			ny = temp.second + dy[i];
+			if (nx >= 1 && ny >= 1 && nx <= N && ny <= M) {
+				if (makeboard[nx][ny] == 0) {
+					check[nx][ny] = check[temp.first][temp.second] + 1;
+					makeboard[nx][ny] = 2;
+					que.push({ nx,ny });
+				}
+			}
+
+		}
+
+//		Debug();
+		que.pop();
+	}
+
+	SafeSum();
+
+}
+
+void setting(int cnt) {
+	/*
+		바로 백트래킹이로구나.
+	*/
+	if (cnt == 3) {
+		BFS();
+		return;
+	}
+
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
+			if (board[i][j] == 0) {
+				board[i][j] = -1;
+				check[i][j] = -1;
+				setting(cnt + 1);//64*64*64 =262,144
+				board[i][j] = 0;
+				check[i][j] = 0;
+			}
+		}
+	}
+
+
+}
+
 
 void enter() {
 	cin >> N >> M;
@@ -72,14 +122,11 @@ void enter() {
 		for (int j = 1; j <= M; j++) {
 			cin >> board[i][j];
 			if (board[i][j] == 2) {
-				que.push({ i,j });
 				board[i][j] = -2;
-				initboard[i][j] = -2;
-//				check[i][j] = 0; 0초 시작
+//				check[i][j] = 0; // 0초에서 출발
 			}
 			else if (board[i][j] == 1) {
 				board[i][j] = -1;
-				initboard[i][j] = -1;
 				check[i][j] = -1;
 			}
 		}
@@ -87,31 +134,10 @@ void enter() {
 
 }
 
-void main() {
+int main() {
 	enter();
-	reset(); //초기화
-	setting();//3개 놓기
-	while (!que.empty()) {
-		PairInt temp = que.front();
-		for (int i = 0; i < 4; i++) {
-			int nx, ny;
-			nx = temp.first + dx[i];
-			ny = temp.second + dy[i];
-			if (nx >= 1 && ny >= 1 && nx <= N && ny <= M) {
-				if (board[nx][ny] == 0) {
-					check[nx][ny] = check[temp.first][temp.second] + 1;
-					board[nx][ny] = 2;
-					que.push({ nx,ny });
-				}
-			}
+	setting(0);//3개 놓기
 
-		}
-
-		Debug();
-		que.pop();
-	}
-
-	SafeSum();
 	cout << safesum;
-	system("pause");
+
 }
