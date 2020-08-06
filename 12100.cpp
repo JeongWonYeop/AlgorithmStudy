d8af74a03a5d45e7ba7d6b96d457b0c2781f32b5
정원엽 <nerbs0313@gmail.com>
2020-08-06 15:49:49 +0900

    12100 merge and move success.

--- 12100.cpp
+++ 12100.cpp
@@ -7,250 +7,90 @@
 #include <functional>
 using namespace std;
 int N;
-int board[20][20];
-//priority_queue <B_I, vector <B_I>, compare1> que1;
-//priority_queue <B_I, vector <B_I>, compare2> que2;
-//priority_queue <B_I, vector <B_I>, compare3> que3;
-int dx[4] = { 0,0,-1,1 }; //좌우위아래
-int dy[4] = { -1,1,0,0 };
-int check[20][20] = { 0, };
-typedef struct board_info {
-	int xx;
-	int yy;
-	int sizee;
-}B_I;
+int board[20][20] = { 0, };
+int checked[20][20] = { 0, };
+int dx[4] = {0,0,-1,1};
+int dy[4] = {-1,1,0,0};
 /*
-00  01
-10  11
+00 01
+10 11
+	기울이기
+		방식 : 나이브
+		시간제한 : O(10^8)
+	합치기
+		방식 : 나이브
+		시간제한 : O(10^8)
+	다섯번만에 얻을 수 있는 가장 큰블록 출력
+		방식 : 백트래킹
+		시간제한 : O(10^8)
+*/
+/*
+	좌,우,위,아래 순서로.
+	0 1 2 3
 */
 
-struct compare0 { //좌측열 기준
-	bool operator()(B_I &a, B_I &c) {
-		if (a.xx != c.xx)
-			return a.xx > c.xx;
-		if (a.yy != c.yy)
-			return a.yy > c.yy;
-		return a.sizee > c.sizee;
+void moveblock(int x,int y,int orient) {
+
+	int nx = x + dx[orient];
+	int ny = y + dy[orient];
+	if (nx < 0 || nx >= N||ny < 0 || ny >= N) {
+		return;
 	}
-};
-
-struct compare1 { //우측열 기준
-	bool operator()(B_I &a, B_I &c) {
-		if (a.xx != c.xx)
-			return a.xx > c.xx;
-		if (a.yy != c.yy)
-			return a.yy < c.yy;
-		return a.sizee > c.sizee;
+	else if (board[nx][ny] == board[x][y] && checked[nx][ny]!=1 && checked[x][y] !=1) {
+		board[nx][ny] = board[x][y] * 2;
+		board[x][y] = 0;
+		checked[nx][ny] = 1;
+		moveblock(nx, ny, orient);
 	}
-};
-
-struct compare2 {//가장 윗행 기준
-	bool operator()(B_I &a, B_I &c) {
-		if (a.yy != c.yy)
-			return a.yy > c.yy;
-		if (a.xx != c.xx)
-			return a.xx > c.xx;
-		return a.sizee > c.sizee;
-	}
-};
-
-struct compare3 { // 가장 아래행 기준
-	bool operator()(B_I &a, B_I &c) {
-		if (a.yy != c.yy)
-			return a.yy > c.yy;
-		if (a.xx != c.xx)
-			return a.xx < c.xx;
-		return a.sizee > c.sizee;
-	}
-};
-
-
-
-priority_queue <B_I, vector <B_I>, compare0> que0; //좌측열기준
-priority_queue <B_I, vector <B_I>, compare1> que1; //우측열기준
-priority_queue <B_I, vector <B_I>, compare2> que2; //위행기준
-priority_queue <B_I, vector <B_I>, compare3> que3; //아래행기준
-void mix(int orient) {
-
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			board[i][j] = 0;
-			check[i][j] = 0;
+	else if (board[nx][ny] == 0) {
+		if (checked[x][y] == 1) {
+			checked[nx][ny] = 1;
+			checked[x][y] = 0;
 		}
+		board[nx][ny] = board[x][y];
+		board[x][y] = 0;
+		moveblock(nx, ny, orient);
 	}
-
-	if (orient == 0) {
-		while (!que0.empty()) {
-			B_I first = que0.top();
-			que0.pop();
-			B_I second = que0.top();
-			que0.pop();
-
-			board[first.xx][first.yy] = first.sizee;
-			board[second.xx][second.yy] = second.sizee;
-
-
-			if (first.xx != second.xx) {
-				que0.push({ second.xx,second.yy,second.sizee });
-				continue;
-			}
-			int nx = first.xx;
-			int nsize = first.sizee;
-			if (board[nx][first.yy] == board[nx][second.yy]) {
-				board[nx][first.yy] =nsize*2;
-				board[second.xx][second.yy] = 0;
-			}
-		}
-	}
-	else if (orient == 1) {
-		while (!que1.empty()) {
-			B_I first = que1.top();
-			que1.pop();
-			B_I second = que1.top();
-			que1.pop();
-
-			board[first.xx][first.yy] = first.sizee;
-			board[second.xx][second.yy] = second.sizee;
-
-
-			if (first.xx != second.xx) {
-				que1.push({ second.xx,second.yy,second.sizee });
-				continue;
-			}
-			int nx = first.xx;
-			int nsize = first.sizee;
-			if (board[nx][first.yy] == board[nx][second.yy]) {
-				board[nx][first.yy] = nsize * 2;
-				board[second.xx][second.yy] = 0;
-			}
-		}
-	}
-	else if (orient == 2) {
-		while (!que2.empty()) {
-			B_I first = que2.top();
-			que2.pop();
-			B_I second = que2.top();
-			que2.pop();
-
-			board[first.xx][first.yy] = first.sizee;
-			board[second.xx][second.yy] = second.sizee;
-
-
-			if (first.yy != second.yy) {
-				que2.push({ second.xx,second.yy,second.sizee });
-				continue;
-			}
-			int samey = first.yy;
-			int nsize = first.sizee;
-			if (board[first.xx][samey] == board[second.xx][samey]) {
-				board[first.xx][samey] = nsize * 2;
-				board[second.xx][samey] = 0;
-			}
-		}
-	}
-	else {
-		while (que3.size() >= 2) {
-			cout << que3.size() << endl;
-			B_I first = que3.top();
-			que3.pop();
-			B_I second = que3.top();
-			que3.pop();
-
-			board[first.xx][first.yy] = first.sizee;
-			board[second.xx][second.yy] = second.sizee;
-
-			cout << first.yy <<" " <<second.yy<<endl;
-			if (first.yy != second.yy) {
-				que3.push({ second.xx,second.yy,second.sizee });
-				continue;
-			}
-			cout << "ㄱㄱ" << endl;
-			int samey = first.yy;
-			int nsize = first.sizee;
-			if (board[first.xx][samey] == board[second.xx][samey]) {
-				board[first.xx][samey] = nsize * 2;
-				board[second.xx][samey] = 0;
-			}
-		}
-	}
-
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			if (orient == 0) {
-				que0.push({ i,j,board[i][j] });
-			}
-			else if (orient == 1)
-				que1.push({ i,j,board[i][j] });
-			else if (orient == 2)
-				que2.push({ i,j,board[i][j] });
-			else 
-				que3.push({ i,j,board[i][j] });
-		}
-	}
-
-
-
 }
+
 void tilt(int orient) {
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			board[i][j] = 0;
+			checked[i][j] = 0;
 		}
 	}
 	if (orient == 0) {
-		while(!que0.empty()){
-			B_I first = que0.top();
-			que0.pop();
-			int ny = first.yy ;
-			while (board[first.xx][--ny] == 0 && ny !=-1) {}
-			board[first.xx][++ny] = first.sizee;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (board[j][i] == 0) continue;
+				else moveblock(j,i, orient);
+			}
 		}
 	}
 	else if (orient == 1) {
-		while (!que1.empty()) {
-			B_I first = que1.top();
-			que1.pop();
-			int ny = first.yy;
-			while (board[first.xx][++ny] == 0 && ny != N) {}
-			board[first.xx][--ny] = first.sizee;
+		for (int i = N-1; i >=0; i--) {
+			for (int j = 0; j < N; j++) {
+				if (board[j][i] == 0) continue;
+				else moveblock(j, i, orient);
+			}
 		}
 	}
 	else if (orient == 2) {
-		while (!que2.empty()) {
-			B_I first = que2.top();
-			que2.pop();
-			int nx = first.xx;
-			while (board[--nx][first.yy] == 0 && nx != -1) {}
-			board[++nx][first.yy] = first.sizee;
+		for (int i =0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (board[i][j] == 0) continue;
+				else moveblock(i, j, orient);
+			}
 		}
 	}
 	else {
-		while (!que3.empty()) {
-			B_I first = que3.top();
-			que3.pop();
-			int nx = first.xx;
-			while (board[++nx][first.yy] == 0 && nx != N) {}
-			board[--nx][first.yy] = first.sizee;
-		}
-	}
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			if (orient == 0) {
-				que0.push({ i,j,board[i][j] });
+		for (int i = N-1; i >= 0; i--) {
+			for (int j = 0; j < N; j++) {
+				if (board[i][j] == 0) continue;
+				else moveblock(i, j, orient);
 			}
-			else if (orient == 1)
-				que1.push({ i,j,board[i][j] });
-			else if (orient == 2)
-				que2.push({ i,j,board[i][j] });
-			else
-				que3.push({ i,j,board[i][j] });
 		}
 	}
-
-
 }
 
 void enter() {
@@ -258,93 +98,23 @@ void enter() {
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
 			cin >> board[i][j];
-			if(board[i][j] != 0){
-				B_I foradd = { i,j,board[i][j] };
-				que0.push(foradd);
-				que1.push(foradd);
-				que2.push(foradd);
-				que3.push(foradd);
-			}
 		}
 	}
 }
 
 
-void see() {
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			cout << board[i][j] << " ";
-		}
-		cout << endl;
-	}
-}
-
-void quesee() {
-	cout << "[que0]좌측열기준" << endl;
-	while (!que0.empty()) {
-		cout << que0.top().xx <<" "<<que0.top().yy<<" "<<que0.top().sizee<< endl;
-		que0.pop();
-	}
-	cout << "[que1]우측열기준" << endl;
-	while (!que1.empty()) {
-		cout << que1.top().xx << " " << que1.top().yy << " " << que1.top().sizee << endl;
-		que1.pop();
-	}
-	cout << "[que2]위행기준" << endl;
-	while (!que2.empty()) {
-		cout << que2.top().xx << " " << que2.top().yy << " " << que2.top().sizee << endl;
-		que2.pop();
-	}
-	cout << "[que3]아래행기준" << endl;
-	while (!que3.empty()) {
-		cout << que3.top().xx << " " << que3.top().yy << " " << que3.top().sizee << endl;
-		que3.pop();
-	}
-
-}
 int main() {
-	/*
-	7.25
-		- 기울이는 기능 ,합치는 기능 필요할 것이다.
-			- 합치는 기능은 먼저 조건검사를 통해서 해당 블록의 블록이 합쳐진 적이 있는지를 먼저 검사한다.
-		- 뭘만들어야할지랑, 시간복잡도만 고려한다.
-
-	7.24
-		·기울이는 기능 ,합치는 기능 필요할 것이다.
-			- 합치는 기능은 먼저 조건검사를 통해서 해당 블록의 블록이 합쳐진 적이 있는지를 먼저 검사한다.
-	
-	7.23
-	자료구조 : 모든 블록의 위치가 담겨있는 큐.
-		큐의 데이터 타입은 구조체로 {int x,int y, int number}.
-		
-
-	기울이기(방향) 함수
-	좌,우,위,아래
-		먼저 충돌 (충돌한 블럭은 빈공백, 충돌된 블럭은 숫자*2) 
-			같은 행이고
-			숫자가 같고
-			사이에 0말고는 없으면
-			이미 한번 합쳐진 블럭인지 검사. -> 
-				아니라면 합쳐.== 숫자*2
-		그리고나서 빈공백 채워주기.
-
-----------------------------
-<백트래킹..? or 배열에 저장하는 방법..?(메모이제이션,dp)>
-
-
-	다섯번 이동해서 최댓값.DFS
-	좌				우				위				아래 O(2^10) => O(10^2)
-좌,우,위,아래	좌,우,위,아래	좌,우,위,아래	좌,우,위,아래
-	*/
-
-
 	enter();
-//	quesee();
-//	tilt(0); // 0:좌,1:우,2:위,3:아래
-	tilt(3);
-	mix(3);
-//	tilt(2);
-//	tilt(3);
-	see();
+	int a = 0;
+	while(a != -1){
+		cin >> a;
+		tilt(a);
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				cout << board[i][j];
+			}
+			cout << endl;
+		}
+	}
 	system("pause");
 }
