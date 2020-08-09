b45803fa1a64ccd5a50bca4efa81694134143d25
정원엽 <nerbs0313@gmail.com>
2020-08-09 14:21:57 +0900

    감시 start

---
+++ 15683.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <string>
+#include <queue>
+#include <functional>
+#include <utility>
+using namespace std;
+
+
+
+
+int main() {
+
+}
