f8bdcd66afdf9b61e7f06a972fb29e8a1e49573c
정원엽 <nerbs0313@gmail.com>
2020-08-10 17:26:50 +0900

    3시간만에 solved

---
+++ 15684.cpp
@@ -0,0 +1,135 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <queue>
+#include <cstring>
+#include <string>
+#include <functional>
+#include <utility>
+using namespace std;
+int N, M, H;
+typedef pair <int, int> PairInt;
+int vec[11][31] = { 0, }; //좌->우
+int vec2[11][31] = { 0, }; //우->좌
+int checked[11][31] = { 0, };
+int ans = 987654321;
+/*
+	사다리 내려가는 함수(출발번호)
+	가로선 서로 겹치는지 아닌지 검사하는 함수.
+	추가하는 가로선이 3 초과가 되면 return, 다 돌았는데도 안되면 -1.
+	추가된 개수가 3초과도 아니고, 모든 i세로선 결과 i면 해당 갯수와 현재까지 최소 갯수를 비교해서 최소갯수 최신화. 
+
+
+*/
+
+
+void enter_line(int x,int y) {
+	vec[x][y] = 1;
+	vec2[x][y + 1] = 1;
+	checked[x][y] = 1;
+	checked[x][y+1] = 1;
+}
+
+void remove_line(int x, int y) {
+	vec[x][y] = 0;
+	vec2[x][y + 1] = 0;
+	checked[x][y] = 0;
+	checked[x][y + 1] = 0;
+}
+
+void enter() {
+	cin >> N >> M >> H; // H행, N열 , 변화되는지점의 갯수 M
+	for (int i = 0; i < M; i++) {
+		int x, y; //y열 x행
+		cin >> x >> y;
+		enter_line(x, y);
+	}
+}
+
+void degug1() {
+	/*
+	for (int i = 1; i < N+1; i++) {
+		if (!vec2[i].empty()) {
+			vector <int> ::iterator v_iter = vec2[i].begin();
+			for (; v_iter != vec2[i].end(); v_iter++) {
+				cout << i << "번째 세로선에" << (*v_iter) << "행에서 변화" << endl;
+			}
+
+		}
+	}
+	*/
+}
+
+int ladder_gotounder(int N,int depth) {//N열, depth 행
+	if (depth == H + 1) return N;
+
+	int n_N = N;
+	
+	if (vec[depth][N] == 1) {
+		n_N = N + 1;
+	}
+	else if(vec2[depth][N] == 1){
+		n_N = N - 1;
+	}
+	ladder_gotounder(n_N, depth + 1);
+}
+
+//겹치면 1 반환
+int is_checked(int x,int y) {
+	if (checked[x][y] == 1) return 1;
+	if (checked[x][y + 1] == 1) return 1;
+	else return 0;
+}
+//모든 i세로선이 i결과.
+int all_pass() {
+	int a = 0;
+	for (int i = 1; i < N + 1; i++) {
+	//	cout << i << "세로선 결과 : " << ladder_gotounder(i, 0) << endl;
+		if (ladder_gotounder(i, 0) == i) {
+			a++;
+		}
+	}
+	if (a == N) return 1;
+	else return 0;
+}
+
+int min(int a,int b) {
+	if (a < b) return a;
+	else return b;
+
+}
+
+
+void dfs(int cnt) {
+	if (all_pass()) {
+		ans = min(ans, cnt-1);
+	//	cout << "<<<<<<<<<<"<<cnt - 1<<">>>>>>>>>>" << endl;
+		return;
+	}
+	if (cnt == 4) return;
+	for (int i = 1; i < H+1; i++) {
+		for (int j = 1; j < N+1; j++) {
+	//		cout << "-------"<<cnt<<"--------" << endl;
+			if (!is_checked(i, j)) {
+				enter_line(i, j);
+	//			cout << i << " " << j<<endl;
+				dfs(cnt + 1);
+				remove_line(i, j);
+			}
+			
+		}
+	}
+
+
+
+
+}
+
+int main() {
+	enter();
+	dfs(1);
+	if (ans == 987654321) ans = -1;
+	cout << ans << endl;
+	system("pause");
+}
