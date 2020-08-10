524e167330f2899a6be2ccf98fb9f61959621988
정원엽 <nerbs0313@gmail.com>
2020-08-10 17:48:19 +0900

    재귀함수는 반환값 int 로 하면 런타임 에러가 나네?

--- 15684.cpp
+++ 15684.cpp
@@ -14,6 +14,7 @@ int vec[11][31] = { 0, }; //좌->우
 int vec2[11][31] = { 0, }; //우->좌
 int checked[11][31] = { 0, };
 int ans = 987654321;
+int goal = 0;
 /*
 	사다리 내려가는 함수(출발번호)
 	가로선 서로 겹치는지 아닌지 검사하는 함수.
@@ -61,9 +62,12 @@ void degug1() {
 	*/
 }
 
-int ladder_gotounder(int N,int depth) {//N열, depth 행
-	if (depth == H + 1) return N;
-
+void ladder_gotounder(int N,int depth) {//N열, depth 행
+	if (depth == H + 1) {
+		goal = N;
+		return;
+	}
+	
 	int n_N = N;
 	
 	if (vec[depth][N] == 1) {
@@ -72,6 +76,7 @@ int ladder_gotounder(int N,int depth) {//N열, depth 행
 	else if(vec2[depth][N] == 1){
 		n_N = N - 1;
 	}
+
 	ladder_gotounder(n_N, depth + 1);
 }
 
@@ -86,7 +91,8 @@ int all_pass() {
 	int a = 0;
 	for (int i = 1; i < N + 1; i++) {
 	//	cout << i << "세로선 결과 : " << ladder_gotounder(i, 0) << endl; 실수 2: 주어진 힌트를 왜 안봤어? 디버깅 과정에서 그 답안지로 문제점을 바로 찾아냈잖아. 그걸 왜 니가 풀고 있냐~
-		if (ladder_gotounder(i, 0) == i) {
+		ladder_gotounder(i, 0);
+		if (goal == i) {
 			a++;
 		}
 	}
@@ -102,14 +108,15 @@ int min(int a,int b) {
 
 
 void dfs(int cnt) {
-	if (cnt == 4) return;
+
 	if (all_pass()) {
 		ans = min(ans, cnt-1);
 	//	cout << "<<<<<<<<<<"<<cnt - 1<<">>>>>>>>>>" << endl;
 		return;
 	}
-	for (int i = 1; i < H; i++) {
-		for (int j = 1; j < N; j++) {
+	if (cnt == 4) return;
+	for (int i = 1; i < H+1; i++) {
+		for (int j = 1; j < N+1; j++) {
 	//		cout << "-------"<<cnt<<"--------" << endl;
 			if (!is_checked(i, j)) {
 				enter_line(i, j);
@@ -130,6 +137,5 @@ int main() {
 	enter();
 	dfs(1);
 	if (ans == 987654321) ans = -1;
-	cout << ans << endl;
-	system("pause");
+	cout << ans;
 }
