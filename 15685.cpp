09413530922380bc3609ad1d71add3b08f198726
정원엽 <nerbs0313@gmail.com>
2020-08-13 11:36:48 +0900

    문제를 이해하는데만 3시간 소요. 세대의 개념을 이해못했고, 이제는 그 세대라는게 신버전 같은 것이라는 걸 알았다.

---
+++ 15685.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <utility>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int main() {
+
+
+}
