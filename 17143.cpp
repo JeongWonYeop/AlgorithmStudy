cb6a90ab9a857747d8d7f284769bef7d25be63f8
정원엽 <nerbs0313@gmail.com>
2020-09-04 15:20:25 +0900

    상어움직임 구현

--- 17143.cpp
+++ 17143.cpp
@@ -25,7 +25,7 @@ using namespace std;
 방향 우
 6- 상어위치 == 6 - 1 == 5 < ((20-4)-5)-5
 방향 좌
-상어위치 - 1 == 6 - 1 == 5 > (((20-4)-5)-5)-5 
+상어위치 - 1 == 6 - 1 == 5 > (((20-4)-5)-5)-5 =1
 방향그대로. 좌, 상어위치에서 빼준다. 6-1 = 5
 
 ----------------------------------------------------
@@ -47,12 +47,47 @@ using namespace std;
 상어위치 - 1 == 6 - 1 == 5 > 11-4-5
 방향그대로 '좌', --> 상어위치에서 빼준다 6 - 2 = '4'
 
+상어 방향이 만약 좌라면,
+스피드가 3이라면,
+상어위치 -1 == 4-1[열시작좌표] == 3 >= 3 라면, 끝.
+	4-3 = 1 방향그대로.
+아니라면 방향바뀌고, ex) 스피드 8. 상어위치-1==5-1 == 3 < 8
+	8- 3가 남고, 6[열끝좌표]- 상어위치 == 6-1 == 5 >= 5 라면. 끝
+		우. 상어위치 더하기 5 == 6.
+
+
+		처음에 방향이 좌라면, 상어위치 - 1 한다음, 그게 만약에 스피드보다 >= 하면 끝. 상어위치 - 스피드. 끝.
+			그게 아니면,상어위치-1 만큼을 스피드에서 빼준다. 그다음, 그 스피드를 가지고
+			열끝좌표 - 열처음좌표 로 / 몫을 구했을 때, 0일경우 방향바뀌고. 상어위치+%한값.
+														1일경우 방향그대로고. 상어위치에서 % 한 값을 빼준다.
+
+		
+		처음에 방향이 우라면, 열끝 - 상어위치 한다음, 그게 만약 스피드보다 >= 하면 끝. 상어위치 + 스피드. 끝.
+			그게 아니면, 열끝-상어위치 만큼을 스피드에서 빼준다. 그다음, 그 스피드를 가지고
+			열끝좌표 - 열처음좌표 로  / 몫을 구했을 때, 0일경우 방향바뀌고. 상어위치에서 % 한 값을 빼준다.
+														1일경우 방향그대로고, 상어위치+%한값.
+														
+														
+		처음에 방향이 위라면,  상어위치 - 1[행처음] 한다음, 그게 만약에 스피드보다 >= 하면 끝. 상어위치 - 스피드. 끝.
+			그게 아니면,상어위치-1 만큼을 스피드에서 빼준다. 그다음, 그 스피드를 가지고
+			열끝좌표 - 열처음좌표 로 / 몫을 구했을 때, 0일경우 방향바뀌고. 상어위치+%한값.
+														1일경우 방향그대로고. 상어위치에서 % 한 값을 빼준다.
+
+
+		처음에 방향이 아래라면, 행끝 - 상어위치 한다음, 그게 만약 스피드보다 >= 하면 끝. 상어위치 + 스피드. 끝.
+			그게 아니면, 열끝-상어위치 만큼을 스피드에서 빼준다. 그다음, 그 스피드를 가지고
+			열끝좌표 - 열처음좌표 로  / 몫을 구했을 때, 0일경우 방향바뀌고. 상어위치에서 % 한 값을 빼준다.
+														1일경우 방향그대로고, 상어위치+%한값.
+
+
+
+
 
 */
 int shark_position[101][101] = { 0, };
 int shark_size[101][101] = { 0, };
 int shark_speed[101][101] = { 0, };
-int shark_orient[101][101] = { 0, };
+int shark_orient[101][101] = { 0, }; //1위 2아래 3우 4좌
 int R, C, M;
 
 
@@ -76,9 +111,208 @@ void debug() {
 	}
 
 }
+int evenn(int a) {
+	if (a % 2 == 0)
+		return 1;
+	else return 0;
+}
 
+void change_orient(int a, int b) {
+	if (shark_orient[a][b] == 1) {
+		shark_orient[a][b] = 2;
+	}
+	else if (shark_orient[a][b] == 2) {
+		shark_orient[a][b] = 1;
+	}
+	else if (shark_orient[a][b] == 3) {
+		shark_orient[a][b] = 4;
+	}
+	else if (shark_orient[a][b] == 4) {
+		shark_orient[a][b] = 3;
+	}
+}
+void shark_move() {
+	for (int i = 1; i <= R; i++) {
+		for (int j = 1; j <= C; j++) {
+			if (shark_position[i][j] > 0) {
+				int t_shark_speed = shark_speed[i][j];
+				if (shark_orient[i][j] == 1) {//위,아래,우,좌
+					cout << "위" << endl;
+					int row = i - 1;
+					if (row >= t_shark_speed) {
+						shark_position[i][j]--;
+						
+						shark_position[i- t_shark_speed][j]++;
+						
+
+						continue;
+					}
+					else {
+						t_shark_speed -= row;
+						if (evenn(t_shark_speed / (R - 1))) {
+							change_orient(i, j);
+							shark_position[i][j]--;
+							cout << t_shark_speed % (R - 1) << endl;
+							shark_position[1 + t_shark_speed % (R-1)][j]++;
+						}
+						else {
+							shark_position[i][j]--;
+							cout << t_shark_speed % (R - 1) << endl;
+							shark_position[R - t_shark_speed % (R-1)][j]++;
+						}
+					}
+				}
+				else if (shark_orient[i][j] == 2) {
+					cout << "아래" << endl;
+					int Row = R - i;
+					if (Row >= t_shark_speed) {
+						cout << "1" << endl;
+						shark_position[i][j]--;
+						shark_position[i + t_shark_speed][j]++;
+						continue;
+					}
+					else {
+						t_shark_speed -= Row;
+						if (evenn(t_shark_speed / (R - 1))) {
+							cout << "2" << endl;
+							change_orient(i, j);
+							shark_position[i][j]--;
+							shark_position[R - t_shark_speed % (R - 1)][j]++;
+						}
+						else {
+							cout << "3" << endl;
+							shark_position[i][j]--;
+							shark_position[1 + t_shark_speed % (R - 1)][j]++;
+						}
+					}
+				}
+				else if (shark_orient[i][j] == 3) {
+					cout << "우" << endl;
+					/*
+					처음에 방향이 우라면, 열끝 - 상어위치 한다음, 그게 만약 스피드보다 >= 하면 끝. 상어위치 + 스피드. 끝.
+					그게 아니면, 열끝-상어위치 만큼을 스피드에서 빼준다. 그다음, 그 스피드를 가지고
+					열끝좌표 - 열처음좌표 로  / 몫을 구했을 때, 0일경우 방향바뀌고. 상어위치에서 % 한 값을 빼준다.
+					1일경우 방향그대로고, 상어위치+%한값.
+														
+					*/
+					int col = C - j;
+						if (col >= t_shark_speed) {
+							shark_position[i][j]--;
+							shark_position[i][j + t_shark_speed]++;
+							continue;
+						}
+						else {
+							t_shark_speed -= col;
+							if (t_shark_speed % (C - 1) == 0) {
+								if (evenn(t_shark_speed / (C - 1))) {//짝수 0 2 4
+									shark_position[i][j]--;
+									shark_position[i][C]++;
+								}
+								else {//홀수 1 3 5
+									change_orient(i, j);
+									shark_position[i][j]--;
+									shark_position[i][1]++;
+								}
+								continue;
+							}
+							if (evenn(t_shark_speed / (C - 1))) {//짝수
+								cout << t_shark_speed / (C - 1) << endl;
+								cout << t_shark_speed << endl;
+								cout << "여기" << endl;
+								change_orient(i, j);
+								shark_position[i][j]--;
+								shark_position[i][C - t_shark_speed % (C - 1)]++;
+							}
+							else { //홀수일 때
+								cout << t_shark_speed / (C - 1) << endl;
+								cout << t_shark_speed << endl;
+								cout << "저기" << endl;
+								shark_position[i][j]--;
+								shark_position[i][1 + t_shark_speed % (C - 1)]++;
+							}
+						}
+
+
+				}else if (shark_orient[i][j] == 4) {
+					cout << "좌" << endl;
+					int col = j - 1;
+					if (col >= t_shark_speed) {
+						shark_position[i][j]--;
+						shark_position[i][j - t_shark_speed]++;
+						continue;
+					}
+					else {
+						t_shark_speed -= col;
+						if (evenn(t_shark_speed / (C-1))) {
+							change_orient(i,j);
+							shark_position[i][j]--;
+							cout << t_shark_speed % (C - 1) << endl;
+							shark_position[i][1 + t_shark_speed % (C-1)]++;
+						}
+						else {
+							shark_position[i][j]--;
+							shark_position[i][C - t_shark_speed % (C-1)]++;
+						}
+					}
+				}
+
+				/*
+					처음에 방향이 좌라면, 상어위치 - 1 한다음, 그게 만약에 스피드보다 >= 하면 끝. 상어위치 - 스피드. 끝.
+					그게 아니면,상어위치-1 만큼을 스피드에서 빼준다. 그다음, 그 스피드를 가지고
+					열끝좌표 - 열처음좌표 로 / 몫을 구했을 때, 0일경우 방향바뀌고. 상어위치+%한값.
+					1일경우 방향그대로고. 상어위치에서 % 한 값을 빼준다.
+
+		
+				
+														
+					처음에 방향이 위라면,  상어위치 - 1[행처음] 한다음, 그게 만약에 스피드보다 >= 하면 끝. 상어위치 - 스피드. 끝.
+					그게 아니면,상어위치-1 만큼을 스피드에서 빼준다. 그다음, 그 스피드를 가지고
+					열끝좌표 - 열처음좌표 로 / 몫을 구했을 때, 0일경우 방향바뀌고. 상어위치+%한값.
+					1일경우 방향그대로고. 상어위치에서 % 한 값을 빼준다.
+
+
+					처음에 방향이 아래라면, 행끝 - 상어위치 한다음, 그게 만약 스피드보다 >= 하면 끝. 상어위치 + 스피드. 끝.
+					그게 아니면, 열끝-상어위치 만큼을 스피드에서 빼준다. 그다음, 그 스피드를 가지고
+					열끝좌표 - 열처음좌표 로  / 몫을 구했을 때, 0일경우 방향바뀌고. 상어위치에서 % 한 값을 빼준다.
+					1일경우 방향그대로고, 상어위치+%한값.
+
+				
+				*/
+			}
+		}
+	}
+}
+/*
+4 6 1
+4 1 10 3 0
+
+4 6 1
+4 1 10 4 0
+
+4 6 1
+4 3 10 2 0
+
+4 6 1
+4 1 10 1 0
+
+
+4 6 8
+4 1 3 3 8
+1 3 5 2 9
+2 4 8 4 1
+4 5 0 1 4
+3 3 1 2 7
+1 5 8 4 3
+3 6 2 1 2
+2 2 2 3 5
+*/
 int main() {
-	enter();
-	debug();
+	while(true){
+		enter();
+		debug();
+
+		shark_move();
+		debug();
+	}
 	system("pause");
 }
