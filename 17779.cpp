e2dcc5716012e5d7625fb94462db8f8f8854cc7f
정원엽 <nerbs0313@gmail.com>
2020-09-23 22:36:51 +0900

    이 시점부터 최근까지 삼성 코테에 시뮬레이션 문제가 많아지는 경향.

---
+++ 17779.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <utility>
+#include <functional>
+#include <string>
+using namespace std;
+
+int main() {
+
+
+}
