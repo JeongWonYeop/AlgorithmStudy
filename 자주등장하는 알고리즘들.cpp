82033ef4c4f3c3328f46cb0a4f65bd470da57dae
정원엽 <nerbs0313@gmail.com>
2020-09-27 21:39:45 +0900

    네이버 코테 이후 update

--- 자주등장하는 알고리즘들.cpp
+++ 자주등장하는 알고리즘들.cpp
@@ -1,10 +1,11 @@
 /*
 ★해당 알고리즘으로 접근하는 문제를 먼저 맨땅에 풀고★, 이론과 이상적인 풀이를 볼 것.
+★dp★
+★ 트리관련 문제(노드 이용하는)★
 투포인터 : https://woovictory.github.io/2019/05/28/Two-Pointer-Algorithm/
 map 자료구조 
 비트마스크 : https://kim6394.tistory.com/246 (비트마스킹 bfs)
 이분탐색 : https://wondy1128.tistory.com/105
-트리dp
 플로이드
 다익스트라 (비트마스크+다익스트라)
 세그트리
